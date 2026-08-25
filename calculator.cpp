4e74f303a6fcc679b89bdc8abb39e591bcbf108e
inas8421 <inas8421@proton.me>
2026-08-25 21:50:14 +0300

    Added helper files from PPP book, files were modified to use without modules feature (because it's not fully worked yet). It is possible to compile this modification with std=c++20 to use examples from book

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include "PPPheaders.h"
+
+int main()
+{
+	cout << "Expression: ";
+	int lval = 0;
+	int rval = 0;
+	char operation =0;
+	
+	cin >> lval >> operation >> rval;
+
+	//calculation here
+
+	cout << "Result: " << endl; 
+}
